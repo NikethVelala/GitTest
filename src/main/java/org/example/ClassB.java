4595cf7c44612df29546fd795ac0da8e39d57140
Niketh Velala <velalaniketh@gmail.com>
2023-03-29 20:11:56 +0530

    added both ClassA and ClassB

--- src/main/java/org/example/ClassB.java
+++ src/main/java/org/example/ClassB.java
@@ -1,2 +1,10 @@
-package org.example;public class ClassB {
+package org.example;
+
+public class ClassB {
+    int x,y;
+    public ClassB(int x, int y){
+    this.x=x;
+    this.y=y;
+}
+    public int multiplication(){return x*y;}
 }
