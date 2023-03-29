2d8b650387e9f13eb4bcc1c6b607cef896a05aa4
Niketh Velala <velalaniketh@gmail.com>
2023-03-29 19:52:45 +0530

    replace the previous commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,5 +4,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Test branch");
+        System.out.println("New line to test amend command");
+        System.out.println("Made new changes");
     }
 }
