f532f0d8c5ed77b3c1593e7569541efc3bbf9368
Niketh Velala <velalaniketh@gmail.com>
2023-03-29 20:10:55 +0530

    added classA

--- src/main/java/org/example/ClassA.java
+++ src/main/java/org/example/ClassA.java
@@ -2,9 +2,11 @@ package org.example;
 
 public class ClassA {
     int x,y;
-    public ClassA(int x, int y){
+    String s;
+    public ClassA(int x, int y,String s){
         this.x=x;
         this.y=y;
+        this.s=s;
     }
     public int sum(){return x+y;}
 }
