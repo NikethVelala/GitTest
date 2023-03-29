5635725e46692fd8f662b154d47eb435200732fe
Niketh Velala <velalaniketh@gmail.com>
2023-03-29 20:00:52 +0530

    added two files

---
+++ src/main/java/org/example/ClassA.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class ClassA {
+    int x,y;
+    public ClassA(int x, int y){
+        this.x=x;
+        this.y=y;
+    }
+    public int sum(){return x+y;}
+}
---
+++ src/main/java/org/example/ClassB.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class ClassB {
+    int x,y;
+    public ClassB(int x, int y){
+        this.x=x;
+        this.y=y;
+    }
+    public int multiplication(){return x*y;}
+}
+
